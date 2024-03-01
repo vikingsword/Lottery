9e3db8545d9561aebb90a9bccbb84658cc612308
vikingar <423141759@qq.com>
2024-03-02 00:16:41 +0800

    strategy

---
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/service/algorithm/BaseAlgorithm.java
@@ -0,0 +1,12 @@
+package cn.itedus.lottery.domain.strategy.service.algorithm;
+
+/**
+ * @author vikingar
+ * @time 2024/3/2 0:15
+ * @description
+ */
+public abstract class BaseAlgorithm implements IDrawAlgorithm{
+
+
+
+}
