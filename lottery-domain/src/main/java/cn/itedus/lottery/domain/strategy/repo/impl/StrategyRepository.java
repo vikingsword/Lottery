ef15c56dfa99d5c094ad4152fc5075cc06a13c7d
vikingar <423141759@qq.com>
2024-03-02 13:51:33 +0800

    repo impl

--- lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/repo/impl/StrategyRepository.java
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/strategy/repo/impl/StrategyRepository.java
@@ -2,21 +2,44 @@ package cn.itedus.lottery.domain.strategy.repo.impl;
 
 import cn.itedus.lottery.domain.strategy.model.aggregrates.StrategyRich;
 import cn.itedus.lottery.domain.strategy.repo.IStrategyRepository;
+import cn.itedus.lottery.infrastructure.dao.IAwardDao;
+import cn.itedus.lottery.infrastructure.dao.IStrategyDao;
+import cn.itedus.lottery.infrastructure.dao.IStrategyDetailDao;
 import cn.itedus.lottery.infrastructure.po.Award;
+import cn.itedus.lottery.infrastructure.po.Strategy;
+import cn.itedus.lottery.infrastructure.po.StrategyDetail;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author vikingar
  * @time 2024/3/2 13:24
  * @description
  */
+@Component
 public class StrategyRepository implements IStrategyRepository {
+
+    @Resource
+    private IStrategyDao strategyDao;
+
+    @Resource
+    private IStrategyDetailDao strategyDetailDao;
+
+    @Resource
+    private IAwardDao awardDao;
+
+
     @Override
     public StrategyRich queryStrategyRich(Long strategyId) {
-        return null;
+        Strategy strategy = strategyDao.queryStrategy(strategyId);
+        List<StrategyDetail> strategyDetails = strategyDetailDao.queryStrategyDetailList(strategyId);
+        return new StrategyRich(strategyId, strategy, strategyDetails);
     }
 
     @Override
     public Award queryAwardInfo(String awardId) {
-        return null;
+        return awardDao.queryAwardInfo(awardId);
     }
 }
