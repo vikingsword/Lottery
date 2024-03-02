d6317712bdc307e8b8fd596baffff3a8c17fafa3
vikingar <423141759@qq.com>
2024-03-02 13:47:48 +0800

    repo

---
+++ lottery-infrastructure/src/main/java/cn/itedus/lottery/infrastructure/dao/IStrategyDetailDao.java
@@ -0,0 +1,13 @@
+package cn.itedus.lottery.infrastructure.dao;
+
+import cn.itedus.lottery.infrastructure.po.StrategyDetail;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface IStrategyDetailDao {
+
+    List<StrategyDetail> queryStrategyDetailList(Long strategyId);
+
+}
