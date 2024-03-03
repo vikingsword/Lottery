6687ac2ded2fd65c3ea50f8ed2d8acb3c84d261a
vikingar <423141759@qq.com>
2024-03-03 22:43:09 +0800

    service repo

---
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/award/repo/impl/AwardRepository.java
@@ -0,0 +1,14 @@
+package cn.itedus.lottery.domain.award.repo.impl;
+
+import cn.itedus.lottery.domain.award.repo.IAwardRepository;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author vikingar
+ * @time 2024/3/3 22:42
+ * @description
+ */
+@Component
+public class AwardRepository implements IAwardRepository {
+
+}
