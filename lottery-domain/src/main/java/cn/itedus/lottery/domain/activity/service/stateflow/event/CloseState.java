13e76c6d82067e547a279667eb58ab185146f7d2
vikingar <423141759@qq.com>
2024-03-05 18:03:51 +0800

    event

---
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/activity/service/stateflow/event/CloseState.java
@@ -0,0 +1,10 @@
+package cn.itedus.lottery.domain.activity.service.stateflow.event;
+
+/**
+ * @author vikingar
+ * @time 2024/3/5 18:03
+ * @description
+ */
+public class CloseState {
+
+}
---
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/activity/service/stateflow/event/OpenState.java
@@ -0,0 +1,9 @@
+package cn.itedus.lottery.domain.activity.service.stateflow.event;
+
+/**
+ * @author vikingar
+ * @time 2024/3/5 18:03
+ * @description
+ */
+public class OpenState {
+}
