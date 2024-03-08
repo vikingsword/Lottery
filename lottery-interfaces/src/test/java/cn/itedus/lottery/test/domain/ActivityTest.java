da40f5ff057221029decbe057bc53cda0729fd10
vikingar <423141759@qq.com>
2024-03-08 21:42:35 +0800

    test

--- lottery-interfaces/src/test/java/cn/itedus/lottery/test/domain/ActivityTest.java
+++ lottery-interfaces/src/test/java/cn/itedus/lottery/test/domain/ActivityTest.java
@@ -27,13 +27,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-/**
- * @description: 活动领域测试
- * @author: 小傅哥，微信：fustack
- * @date: 2021/9/11
- * @github: https://github.com/fuzhengwei
- * @Copyright: 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- */
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ActivityTest {
