da2141d687f342a7f1dade6ed0a86e320987fc21
vikingar <423141759@qq.com>
2024-03-11 22:17:40 +0800

    vo2dto

---
+++ lottery-rpc/src/main/java/cn/itedus/lottery/req/QuantificationDrawReq.java
@@ -0,0 +1,43 @@
+package cn.itedus.lottery.req;
+
+import java.util.Map;
+
+/**
+ * @author vikingar
+ * @time 2024/3/11 21:54
+ * @description
+ */
+public class QuantificationDrawReq {
+
+    /** 用户ID */
+    private String uId;
+    /** 规则树ID */
+    private Long treeId;
+    /** 决策值 */
+    private Map<String, Object> valMap;
+
+    public Long getTreeId() {
+        return treeId;
+    }
+
+    public void setTreeId(Long treeId) {
+        this.treeId = treeId;
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public Map<String, Object> getValMap() {
+        return valMap;
+    }
+
+    public void setValMap(Map<String, Object> valMap) {
+        this.valMap = valMap;
+    }
+
+}
