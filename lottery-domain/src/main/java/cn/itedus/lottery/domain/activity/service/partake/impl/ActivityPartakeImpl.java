78fba72e24faff2822f563cdad9e79688e6be9ba
vikingar <423141759@qq.com>
2024-03-12 11:18:11 +0800

    kafka

--- lottery-domain/src/main/java/cn/itedus/lottery/domain/activity/service/partake/impl/ActivityPartakeImpl.java
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/activity/service/partake/impl/ActivityPartakeImpl.java
@@ -137,4 +137,9 @@ public class ActivityPartakeImpl extends BaseActivityPartake {
 
     }
 
+    @Override
+    public void updateInvoiceMqState(String uId, Long orderId, Integer mqState) {
+        userTakeActivityRepository.updateInvoiceMqState(uId, orderId, mqState);
+    }
+
 }
